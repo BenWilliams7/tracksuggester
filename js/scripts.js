1c460f566f7e39d7b12fa894e92d109c9c50964a
Ben Williams <benjarwilliams@gmail.com>
2017-03-24 13:26:50 -0700

    add all tracks and working selectors

--- js/scripts.js
+++ js/scripts.js
@@ -2,13 +2,45 @@ $(document).ready(function(){
   $("form#track").submit(function(event) {
 
   var aesthetics = $("#aesthetics").val();
+  var type = $("#type").val();
+  var company = $("#company").val();
+  var root = $("#root").val();
+  var media = $("#media").val();
 
-
-  if (aesthetics === "Looks"){
+  if (media === "Couldn't bear it, get me outside"){
+    $("#out").show();
+    $("#ruby").hide();
+    $("#design").hide();
+    $("#csharp").hide();
+    $("#java").hide();
+  }
+  else if (aesthetics === "Looks"){
+    $("#design").show();
+    $("#ruby").hide();
+    $("#java").hide();
+    $("#csharp").hide();
+    $("#out").hide();
+  }
+  else if ((type === "Websites") && company === "Up and coming"){
     $("#ruby").show();
+    $("#design").hide();
+    $("#java").hide();
+    $("#csharp").hide();
+    $("#out").hide();
+  }
+  else if (type === "Up and coming"){
+    $("#java").show();
+    $("#design").hide();
+    $("#ruby").hide();
+    $("#csharp").hide();
+    $("#out").hide();
   }
   else {
-    $("#java").show();
+    $("#csharp").show();
+    $("#design").hide();
+    $("#ruby").hide();
+    $("#java").hide();
+    $("#out").hide();
   }
   event.preventDefault();
   });
